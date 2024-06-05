ef718e72962dbdef915cdd6d6b8aff45c89a4471
LuizTakahashii <luiz.takahashi@grupointegrado.br>
2024-06-05 20:51:04 -0300

    add
    timer

---
+++ aula-tads/src/components/Timer.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+export const Timer = () => {
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds(seconds + 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [seconds]);
+
+    return <div className="seconds">{seconds}</div>
+};
